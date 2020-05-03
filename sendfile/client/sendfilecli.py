57f2ede3bd98ca89755a4e9da583bdce7e7dea1a
a <avidal@csu.fullerton.edu>
2020-05-03 15:40:16 -0700

    Removed uneccessary "design" comments.

--- sendfile/client/sendfilecli.py
+++ sendfile/client/sendfilecli.py
@@ -40,53 +40,6 @@ while True:
 	tokens = userInput.split(" ")
 	numSent = 0
 
-	#if(userInput[0] == "set"):
-	#	fileName = userInput[1]
-		# The file data
-		#fileData = None
-	# Keep sending until all is sent
-	#	while True:
-	#		
-	#		# Read 65536 bytes of data
-	#		fileObj = open(fileName, "r")
-	#		fileData = fileObj.read(65536)
-	#		
-	#		# Make sure we did not hit EOF
-	#		if fileData:
-	#			
-	#				
-	#			# Get the size of the data read
-	#			# and convert it to string
-	#			dataSizeStr = str(len(fileData))
-	#			
-	#			# Prepend 0's to the size string
-	#			# until the size is 10 bytes
-	#			while len(dataSizeStr) < 10:
-	#				dataSizeStr = "0" + dataSizeStr
-	#		
-	#		
-	#			# Prepend the size of the data to the
-	#			# file data.
-	#			fileData = dataSizeStr + fileData	
-	#			
-	#			# The number of bytes sent
-	#			numSent = 0
-	#			
-	#
-	#			# Send the data!
-	#			while len(fileData) > numSent:
-	#				numSent += comSock.send(fileData[numSent:])
-	#		
-	#		# The file has been read. We are done
-	#		else:
-	#			break
-	#
-	#
-	#	print("Sent ", numSent, " bytes.")
-		
-	#elif(userInput[0] == "get"):
-	#elif(userInput[0] == "ls"):
-
 	commandData = userInput	
 	ftp_helper.sendData(comSock, commandData, headerSize)
 	if(tokens[0] == "quit"):
